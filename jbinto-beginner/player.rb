f06ca58ba20c2d160216d9c50255cbde561b01ab
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-11 18:59:18 -0400

    Finish level 4. It's much trickier now.
    - Log all our decisions.
    - Separate complex actions into methods.
    - Strategy: Heal if I'm unhealthy and it's empty in front of me. Unless
      I just took damage (e.g. from an archer), in that case push forward.

--- jbinto-beginner/player.rb
+++ jbinto-beginner/player.rb
@@ -1,21 +1,59 @@
 class Player
+
+  attr_accessor :warrior
+
+  def initialize
+  	@old_health = 20
+  end
+
   def play_turn(warrior)
-  	if warrior.feel.empty?
+  	# make it easier to call methods...
+  	self.warrior = warrior 
+
+  	log "health: #{warrior.health}"
+
+  	if empty_ahead?
   		log "It's empty in front of us."
-  		if warrior.health < 20
-  			puts "I'm hurt, I'll rest for now."
+  		if should_rest?
+  			log "I think I'll rest for now."
   			warrior.rest!
   		else
-  			puts "I'm healthy. I'll walk forward."
+  			log "I'll walk forward."
 			warrior.walk!
 		end
 	else # it's not empty in front of us...
 		log "Attack!"
 		warrior.attack!
 	end
+
+	@old_health = warrior.health
   end
 
   def log(message)
   	puts "--> #{message}"
   end
+
+  def empty_ahead?  	
+  	warrior.feel.empty?
+  end
+
+  def hurt?
+  	warrior.health < 20
+  end
+
+  def should_rest?
+  	log "Should I rest?"
+  	if just_took_damage?
+  		log "I just took damage, so I can't rest!"
+  		false
+  	else
+  		log "Am I hurt? #{hurt?}. Is it empty ahead? #{empty_ahead?}"
+  		hurt? && empty_ahead?
+  	end
+  end
+
+  def just_took_damage?
+  	warrior.health < @old_health
+  end
+
 end
