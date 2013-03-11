ad900bd9fdb01d0a3a217bd2360b60f84234f68c
Jesse Buchanan <jbinto@jbinto.ca>
2013-03-11 18:43:08 -0400

    Finish level 3: If it's safe and I'm unhealthy, rest.

--- jbinto-beginner/player.rb
+++ jbinto-beginner/player.rb
@@ -1,5 +1,21 @@
 class Player
   def play_turn(warrior)
-	warrior.walk!	
+  	if warrior.feel.empty?
+  		log "It's empty in front of us."
+  		if warrior.health < 20
+  			puts "I'm hurt, I'll rest for now."
+  			warrior.rest!
+  		else
+  			puts "I'm healthy. I'll walk forward."
+			warrior.walk!
+		end
+	else # it's not empty in front of us...
+		log "Attack!"
+		warrior.attack!
+	end
+  end
+
+  def log(message)
+  	puts "--> #{message}"
   end
 end
